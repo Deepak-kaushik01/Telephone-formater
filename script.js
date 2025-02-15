44b725d48fd57469176a17fad49c0965ea9cf4df
Deepak Kaushik01 <deep.kaushik898@gmail.com>
2025-02-15 23:03:24 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// let userInput = document.querySelector('input');
+
+// let previous = '';1
+// userInput.addEventListener('input', (e) => {
+//     let userInputValue = e.target.value;
+//     console.log(previous, userInputValue);
+//     if (userInputValue.length === 3) {
+//         userInput.value = `+(${userInputValue.substring(0, 3)}) - ${userInputValue[userInputValue.length - 1]}`;
+//     }
+//     previous = userInputValue
+// })
+
+// function findNeedle(haystack) {
+//   // your code here
+//   for (let i = 0; i <= haystack.length - 1; i++) {
+//     if (haystack[i] === "needle") {
+//       return `found the needle at position ${i}`;
+//     }
+//   }
+// }
+
+// console.log(
+//   findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
+// );
+
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
