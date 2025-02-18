5021eb6c80402f603be410f2674fcce124289838
Deepak Kaushik01 <deep.kaushik898@gmail.com>
2025-02-18 15:56:28 +0530

    new change in js

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-// let userInput = document.querySelector('input');
+let userInput = document.querySelector('input');
 
-// let previous = '';1
-// userInput.addEventListener('input', (e) => {
-//     let userInputValue = e.target.value;
-//     console.log(previous, userInputValue);
-//     if (userInputValue.length === 3) {
-//         userInput.value = `+(${userInputValue.substring(0, 3)}) - ${userInputValue[userInputValue.length - 1]}`;
-//     }
-//     previous = userInputValue
-// })
-
-// function findNeedle(haystack) {
-//   // your code here
-//   for (let i = 0; i <= haystack.length - 1; i++) {
-//     if (haystack[i] === "needle") {
-//       return `found the needle at position ${i}`;
-//     }
-//   }
-// }
-
-// console.log(
-//   findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
-// );
-
-
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-// let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'];
-
-// function alphabetPosition(text) {
-//   }
-
-//   console.log(alphabetPosition('a'))
-
-//   console.log(alphabetPosition("The sunset sets at twelve o' clock."))
+let previous = '';1
+userInput.addEventListener('input', (e) => {
+    let userInputValue = e.target.value;
+    console.log(previous, userInputValue);
+    if (userInputValue.length === 3) {
+        userInput.value = `+(${userInputValue.substring(0, 3)}) - ${userInputValue[userInputValue.length - 1]}`;
+    }
+    previous = userInputValue
+})
